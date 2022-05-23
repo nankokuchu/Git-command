45e4c3c4a3420a54c0d016117b77e079fc738fc9
zzazz <zzazz@gmail.com>
2022-05-23 16:40:16 +0900

    commit

--- src/main/java/com/atzzazz/gitCommand.java
+++ src/main/java/com/atzzazz/gitCommand.java
@@ -5,3 +5,17 @@ public class gitCommand {
         System.out.println("棕棕爱主主");
     }
 }
+/**
+ * git config —global user.name
+ * git config —global user.email
+ * git init
+ * git status
+ * git add 文件名
+ * git commit -m"日志信息" 文件名
+ * git reflog
+ * git reset —hard 版本号
+ * git branch 分支名
+ * git branch -v
+ * git checkout 分支名
+ * git merge 分支名
+ */
